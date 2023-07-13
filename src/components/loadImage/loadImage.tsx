86eba5c36d43ffdcb1206206d9487c33f63e3bcc
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-07-13 15:03:56 -0300

    primera prueba de subida de imagenes

---
+++ src/components/loadImage/loadImage.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import styles from "./loadImage.module.scss";
+//handleDrop
+export default function LoadImages() {
+  const [selectedImage, setSelectedImage] = useState<string>("");
+  // console.log(selectedImage);
+  type DropHanlde = React.DragEvent<HTMLDivElement>
+  
+  const handleDrop = (event:DropHanlde) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    console.log("SOY EL BLOB",file); //!Este es el bloooob
+    if (file && file.type.match("image.*")) {
+      const reader = new FileReader(); //! no nos sirve porque es el base 64
+      
+      reader.onload = () => {
+        reader.result &&
+        typeof reader.result == "string" &&
+        setSelectedImage(reader.result);
+        console.log("SOY EL BASE 64",reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert("El archivo seleccionado no es una imagen.");
+    }
+  };
+
+  const handleDragOver = (event: any) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div
+      className={styles.drop}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+    >
+      {!!selectedImage ? (
+        <img src={selectedImage} alt="Imagen seleccionada" />
+      ) : (
+        "Arrastra y suelta una imagen aquí"
+      )}
+    </div>
+  );
+}
---
+++ src/pages/PRUEBA/index.tsx
@@ -0,0 +1,9 @@
+import LoadImages from "../../components/loadImage/loadImage";
+
+export default function index() {
+  return (
+    <div>
+      <LoadImages/>
+    </div>
+  )
+}
