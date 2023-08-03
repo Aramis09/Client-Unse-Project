0ac6da7c5d0642a189d3d27107a3c23c20adff8f
Aramis jaime <you@example.com>
2023-08-03 16:35:09 -0300

    add styles

--- src/pages/prueba/index.tsx
+++ src/pages/prueba/index.tsx
@@ -14,37 +14,3 @@ export default function index() {
     </div>
   );
 }
-
-const ImageComponent = () => {
-  const handleDelete = async (publicId: string) => {
-    try {
-      const response = await fetch(`/api/delete-image?public_id=${publicId}`, {
-        method: "DELETE",
-      });
-      if (response.ok) {
-        console.log("Imagen eliminada con éxito.");
-        // Actualizar el estado de tu componente o hacer cualquier otra acción necesaria.
-      } else {
-        console.error("Error al eliminar la imagen.");
-      }
-    } catch (error) {
-      console.error("Error al eliminar la imagen:", error);
-    }
-  };
-
-  return (
-    <div>
-      {/* Aquí muestra la imagen */}
-      <Image
-        cloudName="TU_CLOUD_NAME"
-        publicId="PUBLIC_ID_DE_LA_IMAGEN"
-        width="200"
-        height="200"
-        crop="fill"
-      />
-      <button onClick={() => handleDelete("PUBLIC_ID_DE_LA_IMAGEN")}>
-        Eliminar imagen
-      </button>
-    </div>
-  );
-};
