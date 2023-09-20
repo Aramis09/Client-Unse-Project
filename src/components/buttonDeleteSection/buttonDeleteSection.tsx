008d5cc6ddc0630fb814bef70f37759c084c6f7d
Aramis jaime <you@example.com>
2023-09-20 17:13:44 -0300

    fix bugs

--- src/components/buttonDeleteSection/buttonDeleteSection.tsx
+++ src/components/buttonDeleteSection/buttonDeleteSection.tsx
@@ -1,11 +1,14 @@
 import { sendDeleted } from "@/helpers/delete";
+import { verificationToken } from "@/helpers/verifyToken";
 import { ICON_DELETE } from "@/utils/consts";
 import React, { useState } from "react";
+import useVerifyToken from "@/customHooks/useVerifyAdmin";
 interface P {
   styles: string;
   id: number;
 }
 export default function ButtonDeleteSection({ styles, id }: P) {
+  const { statusToken } = useVerifyToken();
   const [deleteCounter, setdeleteCounter] = useState(0);
   const handleDelete = async () => {
     setdeleteCounter(deleteCounter + 1);
@@ -13,10 +16,13 @@ export default function ButtonDeleteSection({ styles, id }: P) {
     sendDeleted({ id, typeDelete: "sections" });
     window.location.reload();
   };
-  return (
+
+  return statusToken ? (
     <button onClick={handleDelete} className={styles}>
       <img src={ICON_DELETE} alt="iconDelete" />
       <p>{deleteCounter}/3</p>
     </button>
+  ) : (
+    <></>
   );
 }
