ecebf7dbd42b489254d75a75bbd55775537ad46f
Aramis jaime <you@example.com>
2023-09-20 16:52:18 -0300

    change

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -69,8 +69,8 @@ export const  ICON_DELETE = "https://res.cloudinary.com/dzqxa7jfj/image/upload/v
 
 // export const NEXT_PUBLIC_DOMAIN_API_REST = "https://servidor-unse.onrender.com"  
 // export const NEXT_PUBLIC_DOMAIN_API_REST = "http://localhost:3001"   
-
 export const NEXT_PUBLIC_DOMAIN_API_REST = "https://fisicafceyt.unse.edu.ar:10443"   
+
 
 export const URL_TO_CREATE_ABOUT = `${NEXT_PUBLIC_DOMAIN_API_REST}/about/createAbout` 
 export const URL_TO_GET_ABOUT = `${NEXT_PUBLIC_DOMAIN_API_REST}/about/getAbout`
