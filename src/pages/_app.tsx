c66805a75a4644cd1f094930e82722b42f198aa0
Aramis jaime <you@example.com>
2023-10-03 00:06:36 -0300

    try one

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,23 +5,14 @@ import Footer from "@/components/footer/footer";
 import dynamic from "next/dynamic";
 import { AnimatePresence, motion } from "framer-motion";
 import variantsFramerMotion from "@/framerMotion/transitionsVars";
+import { ReactNode } from "react";
 
 function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <NavBar />
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={router.asPath}
-          variants={variantsFramerMotion}
-          initial={variantsFramerMotion.initial}
-          animate={variantsFramerMotion.animate}
-          transition={{ ease: "linear", duration: 0.2, x: { duration: 0.2 } }}
-          exit={variantsFramerMotion.extit}
-        >
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
+
+      <Transition children={<Component {...pageProps} />} />
 
       <Footer />
     </>
@@ -29,3 +20,24 @@ function App({ Component, pageProps, router }: AppProps) {
 }
 
 export default dynamic(() => Promise.resolve(App), { ssr: false });
+interface Props {
+  children: ReactNode;
+}
+const Transition: React.FC<Props> = ({ children }) => (
+  <AnimatePresence mode="wait">
+    <motion.div
+      key={location.pathname}
+      variants={variantsFramerMotion}
+      initial={variantsFramerMotion.initial}
+      animate={variantsFramerMotion.animate}
+      transition={{
+        ease: "linear",
+        duration: 0.2,
+        x: { duration: 0.1, type: "spring" },
+      }}
+      exit={variantsFramerMotion.extit}
+    >
+      {children}
+    </motion.div>
+  </AnimatePresence>
+);
